c777981455a002df0776385c549f751e1891d58b
Gianluca Lippolis <gianluca.lippolis@enhancers.it>
2024-09-09 15:25:50 +0200

    added permissions request and declaration for ios

--- example/ios/BackgroundGeofenceExample/AppDelegate.mm
+++ example/ios/BackgroundGeofenceExample/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <UserNotifications/UNUserNotificationCenter.h>
 
 @implementation AppDelegate
 
@@ -10,6 +11,12 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+  [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert + UNAuthorizationOptionSound)
+                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                            // Enable or disable features based on authorization.
+                        }];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
