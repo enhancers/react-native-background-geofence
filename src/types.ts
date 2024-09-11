d29dd7bed4b1d4e000371767a49fabecce99f2d1
Gianluca Lippolis <gianluca.lippolis@enhancers.it>
2024-09-11 12:18:01 +0200

    changed geofence config interface name

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,9 @@
-import { type EmitterSubscription } from 'react-native';
-
 export enum Events {
   EXIT = 'onExitGeofence',
   ENTER = 'onEnterGeofence',
 }
 
-export interface Config {
+export interface GeofenceConfig {
   id: string;
   lat: number;
   lng: number;
@@ -20,19 +18,4 @@ export interface HeadlessTaskEvent {
   name: HeadlessTaskEventName;
   params: any;
 }
-
-export interface BoundaryStatic {
-  on: (
-    event: Events,
-    callback: (boundaries: string[]) => void
-  ) => EmitterSubscription;
-  off: (event: Events) => void;
-  add: (config: Config) => Promise<string>;
-  remove: (id: string) => Promise<null>;
-  removeAll: () => Promise<null>;
-}
-
-declare let Boundary: BoundaryStatic;
-export default Boundary;
-
 type HeadlessTaskEventName = 'onEnterGeofence' | 'onExitGeofence';
